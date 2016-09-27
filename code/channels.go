4dca8d08b6d9dbdb89a03e34cb0486f04975fcf4
carles <carles@carles>
2016-09-27 18:00:32 +0200

    Last changes

--- code/channels.go
+++ code/channels.go
@@ -11,12 +11,14 @@ func sum(s []int, c chan int) {
 }
 
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
-
+	s := []int{7, 2, 8, -9, 4, 0}	
 	c := make(chan int)
+	d := make(chan int)
+	
 	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c
+	go sum(s[len(s)/2:], d)
+
+	x, y := <-c, <-d
 
 	fmt.Println(x, y, x+y)
 }
--- code/variables5.go
+++ code/variables5.go
@@ -6,6 +6,6 @@ func main() {
 	a := 2
 	var c uint = uint(a)
 	
-	fmt.Printf("a is of type %T\n", a)
-	fmt.Printf("c is of type %T\n", c)
+	fmt.Printf("a is of type %T, value: %v\n", a, a)
+	fmt.Printf("c is of type %T, value: %v\n", c, c)
 }
